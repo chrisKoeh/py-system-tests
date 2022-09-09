55a04fe0a44adff193a4ec9be60c8aada9498ac3
chrisKoeh <christian_koehler87@gmx.de>
2022-09-09 13:15:59 -0500

    version bump 0.5.1, refactor testcases from examples/main_http.py into examples/tc_http.py

--- examples/main_http.py
+++ examples/main_http.py
@@ -1,17 +1,4 @@
-from simple_system_tests import *
-import requests  # pip3 install requests
-
-testcases_ = [("gmx.net", 200, "GET"),
-              ("google.de", 405, "POST"),
-              ("gmx.net/roflfoobar", 404, "GET")]
-
-@testcases(testcases_)
-def check_status_code(url_params):
-    url = url_params[0]
-    expected_code = url_params[1]
-    method = url_params[2]
-    r = requests.request(method, "https://" + url)
-    print(r.status_code)
-    assert expected_code == r.status_code
+from simple_system_tests import run_tests
+from tc_http import check_status_code
 
 run_tests()
---
+++ examples/tc_http.py
@@ -0,0 +1,15 @@
+from simple_system_tests import testcases
+import requests  # pip3 install requests
+
+testcases_ = [("gmx.net", 200, "GET"),
+              ("google.de", 405, "POST"),
+              ("gmx.net/roflfoobar", 404, "GET")]
+
+@testcases(testcases_)
+def check_status_code(url_params):
+    url = url_params[0]
+    expected_code = url_params[1]
+    method = url_params[2]
+    r = requests.request(method, "https://" + url)
+    print(r.status_code)
+    assert expected_code == r.status_code
